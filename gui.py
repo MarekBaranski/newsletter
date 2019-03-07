4606d550583d58a39064f49746e2b5fcef157bae
Marek Baranski <marek.baranski@capgemini.com>
2019-03-07 10:05:03 +0100

    bind button CLICK with property entry

--- gui.py
+++ gui.py
@@ -2,26 +2,37 @@ from tkinter import *
 
 
 def addCc():
-    lableCc = Label(center, text='UW:', width=10)
-    lableCc.grid(row=1, column=0, pady=10)
-    entryCc = Entry(center, width=65)
-    entryCc.grid(row=1, column=1, pady=10)
-    clearCcButton = Button(center, text='clear', width=10)
-    clearCcButton.grid(row=1, column=2, padx=20, sticky="ew")
+    def clearEntryCc():
+        entryCc.delete(0, END)
 
+    lableCc = Label(center, text='UW:', width=10)
+    lableCc.grid(row=1, column=0,  pady=10, sticky='ns')
+    entryCc = Entry(center, width=65)
+    entryCc.grid(row=1, column=1, pady=10, sticky='ns')
+    clearCcButton = Button(center, text='clear', width=10, command=clearEntryCc)
+    clearCcButton.grid(row=1, column=2, padx=20)
     ccButton.config(state="disabled")
 
 
-def addBcc():
-    lableBcc = Label(center, text='UDW:', width=10)
-    lableBcc.grid(row=2, column=0, pady=10)
-    entryBcc = Entry(center, width=65)
-    entryBcc.grid(row=2, column=1, pady=10)
-    clearCcButton = Button(center, text='clear', width=10)
-    clearCcButton.grid(row=2, column=2, padx=20, sticky="ew")
 
+def addBcc():
+    def clearEntryBcc():
+        entryBcc.delete(0, END)
+
+    lableBcc = Label(center, text='UDW:', width=10)
+    lableBcc.grid(row=2, column=0, pady=10, sticky='ns')
+    entryBcc = Entry(center, width=65)
+    entryBcc.grid(row=2, column=1, pady=10, sticky='ns')
+    clearBccButton = Button(center, text='clear', width=10, command=clearEntryBcc)
+    clearBccButton.grid(row=2, column=2, padx=20)
     bccButton.config(state="disabled")
 
+    def clearEntryBcc():
+        entryBcc.delete(0, END)
+
+
+def clearEntryTo():
+    entryTo.delete(0, END)
 
 # ------------------------------------
 
@@ -39,9 +50,9 @@ btm_frame = Frame(window, width=450, height=45, pady=3)
 #window.grid_rowconfigure(0, weight=1)
 #window.grid_columnconfigure(0, weight=1)
 
-top_frame.grid(row=0, sticky="ew")
+top_frame.grid(row=0, sticky='W')
 center.grid(row=1, pady=25)
-btm_frame.grid(row=2, sticky="ew")
+btm_frame.grid(row=2, sticky='E')
 
 # create the widgets for the top frame
 sendButton = Button(top_frame, text='Send', width=10)
@@ -59,11 +70,17 @@ attachButton.grid(row=0, column=3)
 # create the center widgets
 lableTo = Label(center, text='DO:', width=10)
 entryTo = Entry(center, width=65)
-clearToButton = Button(center, text='clear', width=10)
+clearToButton = Button(center, text='clear', width=10, command=clearEntryTo)
+
+lableSubject = Label(center, text='Temat:', width=10)
+entrySubject = Entry(center, width=65)
 
 # layout the widgets in the top frame
-lableTo.grid(row=0, column=0, sticky="ns")
-entryTo.grid(row=0, column=1, padx=5, sticky="ns")
-clearToButton.grid(row=0, column=2, padx=20, sticky="ew")
+lableTo.grid(row=0, column=0)
+entryTo.grid(row=0, column=1, padx=5)
+clearToButton.grid(row=0, column=2, padx=20)
+
+lableSubject.grid(row=3, column=0, pady=10)
+entrySubject.grid(row=3, column=1, pady=10, padx=5)
 
 window.mainloop()
