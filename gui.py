f06137382590cbfebfc48b0738c90bc4dfce6b0c
Marek Baranski <marek.baranski@capgemini.com>
2019-03-07 12:42:48 +0100

    first version GUI without bind method

--- gui.py
+++ gui.py
@@ -1,16 +1,16 @@
 from tkinter import *
-
+from tkinter import ttk
 
 def addCc():
     def clearEntryCc():
         entryCc.delete(0, END)
 
     lableCc = Label(center, text='UW:', width=10)
-    lableCc.grid(row=1, column=0,  pady=10, sticky='ns')
+    lableCc.grid(row=2, column=0,  pady=10, sticky='ns')
     entryCc = Entry(center, width=65)
-    entryCc.grid(row=1, column=1, pady=10, sticky='ns')
+    entryCc.grid(row=2, column=1, pady=10, sticky='ns')
     clearCcButton = Button(center, text='clear', width=10, command=clearEntryCc)
-    clearCcButton.grid(row=1, column=2, padx=5)
+    clearCcButton.grid(row=2, column=2, padx=5)
     ccButton.config(state="disabled")
 
 
@@ -20,11 +20,11 @@ def addBcc():
         entryBcc.delete(0, END)
 
     lableBcc = Label(center, text='UDW:', width=10)
-    lableBcc.grid(row=2, column=0, pady=10, sticky='ns')
+    lableBcc.grid(row=3, column=0, pady=10, sticky='ns')
     entryBcc = Entry(center, width=65)
-    entryBcc.grid(row=2, column=1, pady=10, sticky='ns')
+    entryBcc.grid(row=3, column=1, pady=10, sticky='ns')
     clearBccButton = Button(center, text='clear', width=10, command=clearEntryBcc)
-    clearBccButton.grid(row=2, column=2, padx=5)
+    clearBccButton.grid(row=3, column=2, padx=5)
     bccButton.config(state="disabled")
 
     def clearEntryBcc():
@@ -39,20 +39,20 @@ def clearEntryTo():
 
 window = Tk()
 window.title("Newsletter")
-window.geometry('{}x{}'.format(760, 550))
+window.geometry('{}x{}'.format(760, 600))
 
 # create all of the main containers
-top_frame = Frame(window, width=450, height=50, pady=3)
+top_frame = Frame(window, width=450, height=20, pady=3)
 center = Frame(window, width=450, height=50, pady=3)
-btm_frame = Frame(window, width=450, height=45, pady=3)
+btm_frame = Frame(window, width=450, height=10, pady=3)
 
 # layout all of the main containers
 #window.grid_rowconfigure(0, weight=1)
 #window.grid_columnconfigure(0, weight=1)
 
 top_frame.grid(row=0, sticky='W')
-center.grid(row=1, pady=25)
-btm_frame.grid(row=2, sticky='E')
+center.grid(row=1)
+btm_frame.grid(row=2, sticky='NE')
 
 # create the widgets for the top frame
 sendButton = Button(top_frame, text='Send', width=10)
@@ -60,11 +60,13 @@ ccButton = Button(top_frame, text='DW', command=addCc, width=10)
 bccButton = Button(top_frame, text='UDW', command=addBcc,  width=10)
 attachButton = Button(top_frame, text='Załącz', width=10)
 
+
 # layout the widgets in the top frame
 sendButton.grid(row=0, column=0)
 ccButton.grid(row=0, column=1)
 bccButton.grid(row=0, column=2)
 attachButton.grid(row=0, column=3)
+
 
 
 # create the center widgets
@@ -83,21 +85,34 @@ labelParagraphOne = Label(center, text='Tekst:', width=12)
 textParagraphOne = Text(center, height=5, width=52, yscrollcommand=sb_textbox.set)
 sb_textbox.config(command=textParagraphOne.yview) #bind yview method to scrollbar
 
+separatorTop = ttk.Separator(center, orient=HORIZONTAL)
+separator = ttk.Separator(center, orient=HORIZONTAL)
 
-# layout the widgets in the top frame
-lableTo.grid(row=0, column=0)
-entryTo.grid(row=0, column=1)
-clearToButton.grid(row=0, column=2, padx=5)
 
-lableSubject.grid(row=3, column=0, pady=10)
-entrySubject.grid(row=3, column=1, pady=10, padx=5)
+# layout the widgets in the center frame
+lableTo.grid(row=1, column=0)
+entryTo.grid(row=1, column=1)
+clearToButton.grid(row=1, column=2, padx=5)
 
-lableWelcome.grid(row=4, column=0, pady=30)
-entryWelcome.grid(row=4, column=1, pady=30)
+lableSubject.grid(row=4, column=0, pady=10)
+entrySubject.grid(row=4, column=1, pady=10, padx=5)
 
-labelParagraphOne.grid(row=5, column=0, pady=10, sticky='N')
-textParagraphOne.grid(row=5, column=1, padx=10, pady=10, sticky='W')
-sb_textbox.grid(row=5, column=2, padx=10, pady=10, sticky='W') #place for scrollbar
+lableWelcome.grid(row=5, column=0, pady=30)
+entryWelcome.grid(row=5, column=1, pady=30)
+
+labelParagraphOne.grid(row=6, column=0, pady=10, sticky='N')
+textParagraphOne.grid(row=6, column=1, padx=10, pady=10, sticky='W')
+sb_textbox.grid(row=6, column=2, padx=10, pady=10, sticky='W') #place for scrollbar
 sb_textbox.place(in_=textParagraphOne, relx=1, rely=0, relheight=1)#size and place scrollbar according with textbox
+
+separatorTop.grid(row=0, columnspan=5, pady=20, padx=10, sticky='ew')
+separator.grid(row=7, columnspan=5, pady=20, padx=10, sticky='ew')
+
+# create the btm frame widgets
+previewButton = Button(btm_frame, text='podgląd', width=10)
+
+# layout the widgets in the btm frame
+previewButton.grid(row=0, column=2)
+
 
 window.mainloop()
